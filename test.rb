c4485582dd7bc9ff92f6a0208fc9db72378eab61
Charlie Barth <charliepbarth@gmail.com>
2020-07-16 20:45:44 +0000

    Done.

--- test.rb
+++ test.rb
@@ -1,26 +1,28 @@
-require 'pry'
-
 class Artist
   attr_accessor :name
   attr_reader :songs
-  
-  extend Memorable::ClassMethods
-  include Memorable::InstanceMethods
 
-  @@all = []
+  @@artists = []
 
   def initialize
-    @@all << self
+    @@artists << self
     @songs = []
   end
-  
+
   def self.find_by_name(name)
-    @@all.detect{|a| a.name == name}
+    @@artists.detect{|a| a.name == name}
   end
 
-  
   def self.all
-    @@all
+    @@artists
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def self.count
+    self.all.count
   end
 
   def add_song(song)
@@ -31,5 +33,8 @@ class Artist
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
-  
+
+  def to_param
+    name.downcase.gsub(' ', '-')
+  end
 end
